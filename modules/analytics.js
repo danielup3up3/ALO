7ee6d64fb304b8db16c90e6c91e7699715562a68
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2018-04-02 23:02:22 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -140,8 +140,8 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/^戰鬥002$/) != null) return exports.funny.Hari2() ;
 	
 	if (trigger.match(/^戰鬥003|^吉貝爾easy/) != null) return exports.funny.Gbear() ;
-	
-	
+	//風之世界樹守衛(初級)
+	if (trigger.match(/^戰鬥B01$/) != null) return exports.funny.easyKaze() ;
 	
 	//////////////////////////怪物實戰設定區////////////////////////////////
 	
