103287f93a5e2ef6110995c241371296ccfa1d81
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2018-04-02 19:32:52 +0800

    Update gachaC.js

--- roll/gachaC.js
+++ roll/gachaC.js
@@ -11,25 +11,25 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 		
 		//宣告角色清單
 		let CharacterList0 = [];//特殊角色清單
-		let CharacterList1 = [];//一星角色清單
-		let CharacterList2 = [];//二星角色清單
-		let CharacterList3 = [];//三星角色清單
-		let CharacterList4 = [];//四星角色清單
-		let CharacterList5 = [];//五星角色清單
+		let CharacterList1 = [];//N角色清單
+		let CharacterList2 = [];//NR角色清單
+		let CharacterList3 = [];//R角色清單
+		let CharacterList4 = [];//SR角色清單
+		let CharacterList5 = [];//SSR角色清單
  
 		var characterChance0 = 0;//特殊角色獲得率
-		var characterChance1 = 0;//一星角色獲得率
-		var characterChance2 = 0;//二星角色獲得率
-		var characterChance3 = 0;//三星角色獲得率
-		var characterChance4 =0 ;//四星角色獲得率
-		var characterChance5 =0; //五星角色獲得率
+		var characterChance1 = 0;//N角色獲得率
+		var characterChance2 = 0;//NR角色獲得率
+		var characterChance3 = 0;//R角色獲得率
+		var characterChance4 =0 ;//SR角色獲得率
+		var characterChance5 =0; //SSR獲得率
 		var characterST = 0;//確認保底角色數量
 		///
 		
 		///確定抽獎狀態
 		if(DrawPool == 0){
 			CharacterList0.length = 3;
-			CharacterList0 = ['疾走艾恩','獵人哈里','菲露米雅'];
+			CharacterList0 = ['克萊因(火)','亞絲娜(水)','莉法(風)'];
 			
 			characterChance0 = 100;
 			characterChance1 = 0;
@@ -43,27 +43,26 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 	
 			}else if(GachaTimes == null){
 				
-				rply.text = '【首次限定！】起始降臨-和最初的夥伴相遇！ \
+				rply.text = '【首次限定！】起始招募-最初的夥伴 \
 					\n\
 					\n 出現稀有度一覽： \
-					\n ★(特殊稀有角色)：100%\
+					\n 必中SR：100%\
 					\n\
 					\n 出現角色一覽:\
-					\n 御三家系列 \
-					\n  疾走艾恩\
-					\n  獵人哈里\
-					\n  菲露米雅\
+					\n  克萊因(火)\
+					\n  亞絲娜(水)\
+					\n  莉法(風)\
 					\n\
-					\n 提供降臨方式：\
+					\n 提供招募方式：\
 					\n 首抽 無需星輝石[一名玩家限定一次] \
 					\n\
-					\n 輸入 降臨 1 內容 即可確認降臨會登場的角色\
-					\n 想要降臨的話，請輸入 降臨 0 降臨方式(首抽) ';
+					\n 輸入 夥伴招募 1 內容 即可確認招募會登場的角色\
+					\n 想要招募的話，請輸入 夥伴招募 0 招募方式(首抽) ';
 				
 				return rply;
 				
 			  }else{
-				rply.text = '本降臨無法使用' + GachaTimes +'降臨喔\n 如果想看本降臨詳細內容，請輸入 [招募 ' + DrawPool + ']';
+				rply.text = '本招募無法使用' + GachaTimes +'招募喔\n 如果想看本招募詳細內容，請輸入 [招募 ' + DrawPool + ']';
 				return rply;	
 			  	}
 		}else if(DrawPool == 1){
@@ -87,23 +86,25 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			if(GachaTimes =='單抽'){
 				times = 1;
 	
-			}else if(GachaTimes =='十連'){
+			}else if(GachaTimes =='十一連'){
 				times = 11;
 				characterST = 1;
 	
 			}else if(GachaTimes == null){
 				
-				rply.text = '【首次限定！】新手招募-絕對獲得攻擊型夥伴一名喔！ \
+				rply.text = '【妖精之舞】第一彈主題卡池 SSR出貨率翻倍!\
 					\n\
-					\n 出現夥伴一覽： \
-					\n 001起始英雄系列 \
-					\n  克雷特\
-					\n  路卡\
-					\n  露\
-					\n  (三名夥伴獲得機率相同)\
+					\n 提供割合： \
+					\n 妖精之舞角色系列 \
+					\n  SSR伊芙(水)\
+					\n  SSR西澤爾(闇)\
+                                        \n  SSR齊維(土)\
+					\n  SSR蓮和(火)\
+                                        \n  SSR姌夜(風)\
 					\n\
 					\n 提供招募方式：\
-					\n 單抽 無需奇蹟石(100%出現夥伴)[一名玩家限定一次] \
+					\n 單抽 需5星輝石\
+                                        \n 十一連 需50星輝石\
 					\n\
 					\n 想要招募的話，請輸入 [招募 0 招募方式] ';
 				
@@ -117,9 +118,9 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			
 			rply.text = '【降臨目錄】目前的降臨一覽表 \
 				\n\
-				\n  0 【起始降臨(首抽)】 \
-				\n  1 【限定降臨】限時卡池「貝爾洛斯」(NEW) \
-				\n  2 【通常降臨】常駐星輝石降臨 \
+				\n  0 【起始招募(首抽)】 \
+				\n  1 【限定招募】第一彈主題卡池「妖精之舞」(NEW) \
+				\n  2 【通常招募】常駐星輝石招募 \
 				\n\
 				\n 如果想看詳細招募內容，請輸入 [招募 招募編號]';
 				return rply;
@@ -130,9 +131,9 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 				\n\
 				\n【降臨目錄】目前的降臨一覽表 \
 				\n\
-				\n  0 【起始降臨(首抽)】 \
-				\n  1 【限定降臨】限時卡池「貝爾洛斯」(NEW) \
-				\n  2 【通常降臨】常駐星輝石降臨 \
+				\n  0 【起始招募(首抽)】 \
+				\n  1 【限定招募】第一彈主題卡池「妖精之舞」(NEW) \
+				\n  2 【通常招募】常駐星輝石招募 \
 				\n\
 				\n 如果想看詳細招募內容，請輸入 [招募 招募編號]';
 				return rply;
@@ -160,17 +161,17 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			if(temp > characterChance4+characterChance5){
 				
 				CharacterResult[times-characterST+i] = CharacterList3[Math.floor((Math.random() * (CharacterList3.length)) + 0)];
-				GachaResult[times-characterST+i] = '\[保底]☆☆☆:' +  CharacterResult[times-characterST+i]+ '\n';
+				GachaResult[times-characterST+i] = '\【R】:' +  CharacterResult[times-characterST+i]+ '\n';
 				
 			}else if(temp <= characterChance4+characterChance5 && temp > characterChance5){
 				
 				CharacterResult[times-characterST+i] = CharacterList4[Math.floor((Math.random() * (CharacterList4.length)) + 0)];
-				GachaResult[times-characterST+i] = '\[保底]☆☆☆☆:' +  CharacterResult[times-characterST+i]+ '\n';
+				GachaResult[times-characterST+i] = '\【SR】:' +  CharacterResult[times-characterST+i]+ '\n';
 				
 			}else if(temp <=characterChance5){
 				
 				CharacterResult[times-characterST+i] = CharacterList5[Math.floor((Math.random() * (CharacterList5.length)) + 0)];
-				GachaResult[times-characterST+i] = '\[保底]☆☆☆☆☆:' +  CharacterResult[times-characterST+i]+ '\n';
+				GachaResult[times-characterST+i] = '\【SSR】:' +  CharacterResult[times-characterST+i]+ '\n';
 			}
 			
 		}//保底腳色處理
@@ -182,32 +183,32 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			if(temp >characterChance1+characterChance2+characterChance3+characterChance4+characterChance5){
 				
 				CharacterResult[i] = CharacterList0[Math.floor((Math.random() * (CharacterList0.length)) + 0)];
-				GachaResult[i] = '\★:' +  CharacterResult[i]+ '\n';
+				GachaResult[i] = '\【SR】:' +  CharacterResult[i]+ '\n';
 				
 			}else if(temp <=characterChance1+characterChance2+characterChance3+characterChance4+characterChance5 && temp >characterChance2+characterChance3+characterChance4+characterChance5){
 				
 				CharacterResult[i] = CharacterList1[Math.floor((Math.random() * (CharacterList1.length)) + 0)];
-				GachaResult[i] = '\☆:' +  CharacterResult[i]+ '\n';
+				GachaResult[i] = '\【N】:' +  CharacterResult[i]+ '\n';
 				
 			}else if(temp <=characterChance2+characterChance3+characterChance4+characterChance5 && temp > characterChance3+characterChance4+characterChance5){
 				
 				CharacterResult[i] = CharacterList2[Math.floor((Math.random() * (CharacterList2.length)) + 0)];
-				GachaResult[i] = '\☆☆:' +  CharacterResult[i]+ '\n';
+				GachaResult[i] = '\【NR】:' +  CharacterResult[i]+ '\n';
 				
 			}else if(temp <= characterChance3+characterChance4+characterChance5 && temp > characterChance4+characterChance5){
 				
 				CharacterResult[i] = CharacterList3[Math.floor((Math.random() * (CharacterList3.length)) + 0)];
-				GachaResult[i] = '\☆☆☆:' +  CharacterResult[i]+ '\n';
+				GachaResult[i] = '\【R】:' +  CharacterResult[i]+ '\n';
 				
 			}else if(temp <= characterChance4+characterChance5 && temp >characterChance5){
 				
 				CharacterResult[i] = CharacterList4[Math.floor((Math.random() * (CharacterList4.length)) + 0)];
-				GachaResult[i] = '\☆☆☆☆:' +  CharacterResult[i]+ '\n';
+				GachaResult[i] = '\【SR】:' +  CharacterResult[i]+ '\n';
 				
 			}else if(temp <= characterChance5){
 				
 				CharacterResult[i] = CharacterList5[Math.floor((Math.random() * (CharacterList5.length)) + 0)];
-				GachaResult[i] = '\☆☆☆☆☆:' +  CharacterResult[i]+ '\n';
+				GachaResult[i] = '\【SSR】:' +  CharacterResult[i]+ '\n';
 				
 			}
 		}//通常腳色處理	
@@ -230,7 +231,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			
 		}///重複角色判斷
 		
-		let GResult ='降臨結果:\n'
+		let GResult ='招募結果:\n'
 		for(var i = 0;i<times;i++){
 			GResult = GResult + GachaResult[i];
 		}
