ed92adcfad070898da097eeac3d2314406374734
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2018-04-02 23:05:36 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -149,6 +149,7 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/^回合結束001$/) != null) return exports.MonsterHariTest.main(mainMsg[1]);
 	if (trigger.match(/^回合結束002$/) != null) return exports.MonsterHariTest2.main(mainMsg[1]);
 	if (trigger.match(/^回合結束003$/) != null) return exports.MonsterGbear.main(mainMsg[1]);
+	if (trigger.match(/^回合結束B01$/) != null) return exports.MonstereasyKaze.main(mainMsg[1]);
 	
 	/*tarot 指令
 	if (trigger.match(/猜拳/) != null) {
