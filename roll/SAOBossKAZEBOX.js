e4b11779af317d4f14d7d184df920b54875ee298
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2018-04-03 19:39:58 +0800

    Update SAOBossKAZEBOX.js

--- roll/SAOBossKAZEBOX.js
+++ roll/SAOBossKAZEBOX.js
@@ -2,7 +2,7 @@ var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
 //////////////// 
-	function main(type) {
+	function main(stype) {
 		
 		let AttackResult = [];//結果
 		let SkillResult = [];
@@ -18,7 +18,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 		var SpecialChance = 0;//特殊技能機率
     
    
-    if(type =='0'){
+    if(stype =='0'){
 			//綠血區域時
 			times = 1
 	    
@@ -39,7 +39,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			SpecialSkill = ['【SSR】風迷宮武=暴風之槍','【SSR】風迷宮武=守護盾劍','【SSR】風迷宮武=恩惠之杖','【SSR】迷宮武=狂嵐之劍'];
 			
       
-      }else if(type =='1'){
+      }else if(stype =='1'){
 			//黃血區域
 		times = 3
 	      
@@ -59,7 +59,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			SpecialSkill = ['【SSR】風迷宮武=暴風之槍','【SSR】風迷宮武=守護盾劍','【SSR】風迷宮武=恩惠之杖','【SSR】迷宮武=狂嵐之劍'];
 		
     
-    }else if(type =='2'){
+    }else if(stype =='2'){
 			//紅血區域
 			times = 5
 	    
@@ -78,7 +78,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			SpecialSkill.length = 4;
 			SpecialSkill = ['【SSR】風迷宮武=暴風之槍','【SSR】風迷宮武=守護盾劍','【SSR】風迷宮武=恩惠之杖','【SSR】迷宮武=狂嵐之劍'];
       
-      }else if(type =='3'){
+      }else if(stype =='3'){
 				//設定說明文內容
 				rply.text ='目前\
 				\n 還沒有EX獎勵';
