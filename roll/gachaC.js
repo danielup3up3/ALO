6db3c2b44fa8c67d99541678e50d3f1ea854f0d1
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2018-04-04 23:44:04 +0800

    Update gachaC.js

--- roll/gachaC.js
+++ roll/gachaC.js
@@ -75,8 +75,8 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 			CharacterList4.length = 3;
 			CharacterList4= ['克萊因(火)','亞絲娜(水)','莉法(風)'];
 			CharacterList5.length = 8;
-			CharacterList5= ['伊芙(水)','西澤爾(闇)','西澤爾(闇)','西澤爾(闇)','齊維(土)','蓮和(火)','姌夜(風)','桐人(無)'];
-			
+			CharacterList5= ['桐人(無)','桐人(無)','桐人(無)','桐人(無)','桐人(無)','桐人(無)','桐人(無)','桐人(無)'];
+			//'伊芙(水)','西澤爾(闇)','西澤爾(闇)','西澤爾(闇)','齊維(土)','蓮和(火)','姌夜(風)','桐人(無)'
 			characterChance1 = 35;
 			characterChance2 = 25;
 			characterChance3 = 20;
